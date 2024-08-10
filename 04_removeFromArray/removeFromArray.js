59c3a348ccc83debbd5cf8e64b104fe57d908119
matthew <mashh@live.co.uk>
2024-08-10 20:12:14 +0100

    removed the filteredArray const and its return, and return the array.filter() instead

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,7 @@
 const removeFromArray = function (array, ...valueToRemove) {
-  const filteredArray = array.filter((element) => {
+  return array.filter((element) => {
     return !valueToRemove.includes(element);
   });
-  return filteredArray;
 };
 
 // Do not edit below this line
