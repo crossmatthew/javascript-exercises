2de4b89808afde21e6b53b1c8fc386da6c7fe7a5
matthew <mashh@live.co.uk>
2024-08-11 00:35:20 +0100

    began creating the fn to sumAll numbers, whether largest is first or not

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(firstNumber, secondNumber) {
+    let sumTotal = 0;
+    if (firstNumber < secondNumber) {
+        for (firstNumber; firstNumber <= secondNumber; firstNumber++) {
+            sumTotal+= firstNumber
+        }
+    } else {
+        for (secondNumber; secondNumber <= firstNumber; secondNumber++) {
+        sumTotal += secondNumber;
+        }
+    }
+    return sumTotal
 };
 
 // Do not edit below this line
