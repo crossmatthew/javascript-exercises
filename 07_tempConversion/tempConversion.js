7a5be6f24e2b875b7fb10002c7b3b58c4a601f8d
matthew <mashh@live.co.uk>
2024-08-11 11:51:36 +0100

    added comments to each fn explaining the conversion formulae. /n Created the convertToCelsius fn.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreesInFahrenheit) {
+  /*  Formula to convert Fahrenheit to Celsius:
+      x = Degrees in F
+      x = (x - 32) * 5 / 9 
+  */
+ const celsiusConversion = (degreesInFahrenheit - 32) * 5 / 9;
+ return Number(celsiusConversion.toFixed(1));
 };
 
 const convertToFahrenheit = function() {
+  /*  Formula to convert Fahrenheit to Celsius:
+      x = Degrees in C
+      x = (x * 9 / 5 + 32)
+  */
 };
 
 // Do not edit below this line
