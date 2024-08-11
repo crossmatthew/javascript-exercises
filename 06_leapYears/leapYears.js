2d057d60e50d8e81145331ddecbf4bcc386449c9
matthew <mashh@live.co.uk>
2024-08-11 11:00:49 +0100

    added a comment to explain the conditions of a leap year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,8 @@
 const leapYears = function(year) {
+    /*  Leap years are divisble by 4 
+        Century years (e.g. 1700) are not leap years
+        unless they are divisible by 400 (e.g. 1600)
+    */
     if (year.toString().endsWith('00') && year % 400 === 0 || !year.toString().endsWith('00') && year % 4 === 0) {
         return true
     } else {
