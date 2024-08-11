6d7794ba98ea5a284b14b681f4fd2584436f6a8a
matthew <mashh@live.co.uk>
2024-08-11 10:36:21 +0100

    moved if conditions on to same line

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,9 +1,7 @@
 const sumAll = function(min, max) {
     let sumTotal = 0;
-    if (!Number.isInteger(min) ||
-        !Number.isInteger(max) ||
-        min < 0 ||
-        max < 0) {
+    if (!Number.isInteger(min) || !Number.isInteger(max) ||
+        min < 0 || max < 0) {
             return 'ERROR'
     }
     if (min > max) [min, max] = [max, min];
