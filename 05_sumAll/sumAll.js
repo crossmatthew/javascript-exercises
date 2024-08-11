1042be228076b204a63420790881bb61f0cefa22
matthew <mashh@live.co.uk>
2024-08-11 10:01:44 +0100

    added if conditions to check typeof data, for whole numbers and for numbers smaller than 0

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
 const sumAll = function(firstNumber, secondNumber) {
     let sumTotal = 0;
+    if (
+        typeof firstNumber !== 'number' || 
+        typeof secondNumber !== 'number' || 
+        firstNumber % 1 !== 0 ||
+        secondNumber % 1 !== 0 ||
+        firstNumber < 0 ||
+        secondNumber < 0) {
+            return 'ERROR'
+    }
     if (firstNumber < secondNumber) {
         for (firstNumber; firstNumber <= secondNumber; firstNumber++) {
             sumTotal+= firstNumber
